820d8216faf5fe9bd65e1b7ccc7ecb1d0b338afc
Claudemyr <claudemyr.medeiros@escolar.ifrn.edu.br>
2025-01-09 23:14:33 -0300

    criando .env

--- spanewtour/src/main.jsx
+++ spanewtour/src/main.jsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import Carrocel from './Carrocel.jsx'
-import Cards from './Cards.jsx'
-import SidebarMenu from './SidebarMenu.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
